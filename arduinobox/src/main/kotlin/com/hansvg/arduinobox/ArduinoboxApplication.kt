854c2c031c5a1952f3da1a760f7d6a1cac82b402
Hans Von Gruenigen <hans.vongruenigen@gmail.com>
2024-01-03 08:32:59 -0500

    Initial commit

---
+++ arduinobox/src/main/kotlin/com/hansvg/arduinobox/ArduinoboxApplication.kt
@@ -0,0 +1,11 @@
+package com.hansvg.arduinobox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArduinoboxApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArduinoboxApplication>(*args)
+}
---
+++ arduinobox/src/test/kotlin/com/hansvg/arduinobox/ArduinoboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hansvg.arduinobox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArduinoboxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
